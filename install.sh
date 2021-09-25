9111df7f720de44cd4fe41b98003438911cf685f
MuratovAS <lexamzx@gmail.com>
2021-09-25 13:39:41 +0500

    Updates to the new version of packages, Switching to MX3

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-chmod 744 ./logitech-master-mx2
+chmod 744 ./logitech-maste
 
-mkdir -p /etc/logitech-master-mx2/
-rm -f /etc/logitech-master-mx2/config
+mkdir -p /etc/logitech-master/
+rm -f /etc/logitech-master/config
 
 echo '
 #!/bin/bash
@@ -11,18 +11,18 @@ EVENT_SIDE2="ydotool key ctrl+alt+shift+a"
 EVENT_SCROLL_UP="ydotool key ctrl+alt+shift+w"
 EVENT_SCROLL_DOWN="ydotool key ctrl+alt+shift+q"
 EVENT_THUMB="ydotool key ctrl+alt+shift+z"
-' >> /etc/logitech-master-mx2/config
+' >> /etc/logitech-master/config
 
-cp logitech-master-mx2 /usr/bin/
-cp logitech-master-mx2.service /usr/lib/systemd/system/
-systemctl enable logitech-master-mx2.service 
-systemctl start logitech-master-mx2.service 
+cp logitech-master /usr/bin/
+cp logitech-master.service /usr/lib/systemd/system/
+systemctl enable logitech-master.service 
+systemctl start logitech-master.service 
 
 cp ydotoold.service /usr/lib/systemd/system/ 
 systemctl enable ydotoold.service 
 systemctl start ydotoold.service 
 
 
-cat /etc/logitech-master-mx2/config
-systemctl status logitech-master-mx2.service 
+cat /etc/logitech-master/config
+systemctl status logitech-master.service 
 systemctl status ydotoold.service 
--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-systemctl disable logitech-master-mx2.service 
-systemctl stop logitech-master-mx2.service 
+systemctl disable logitech-master-mx3.service 
+systemctl stop logitech-master-mx3.service 
 
 systemctl disable ydotoold.service 
 systemctl stop ydotoold.service 
 
 rm /usr/lib/systemd/system/ydotoold.service 
-rm /usr/lib/systemd/system/logitech-master-mx2.service
+rm /usr/lib/systemd/system/logitech-master-mx3.service
 
 
-rm  /etc/logitech-master-mx2/config
-rmdir  /etc/logitech-master-mx2/
-rm  /usr/bin/logitech-master-mx2
+rm  /etc/logitech-master-mx3/config
+rmdir  /etc/logitech-master-mx3/
+rm  /usr/bin/logitech-master-mx3
